bacaf015098a076d2f7f29be3ba920a9df6db000
NamaWorks <pedronayamartinez@hotmail.com>
2025-06-11 22:44:01 +0200

    prepare webpack configuration

---
+++ webpack.config.build.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+const { merge } = require('webpack-merge')
+const config = require('./webpack.config')
+
+module.exports = merge(config, {
+  mode: 'production',
+
+  output: {
+    path: path.join(__dirname, 'public')
+  }
+})
+const path = require('path')
+
+const { merge } = require('webpack-merge')
+const config = require('./webpack.config')
+
+module.exports = merge(config, {
+  mode: 'production',
+
+  output: {
+    path: path.join(__dirname, 'public')
+  }
+})
